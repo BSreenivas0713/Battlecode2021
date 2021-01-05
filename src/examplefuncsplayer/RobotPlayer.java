2231af1e76c8f0463be26a924f81aaa072f3e18d
maxwelljones14 <56135118+maxwelljones14@users.noreply.github.com>
2021-01-05 16:30:52 -0500

    Naive update of runEnlightenmentCenter
    bids a tenth of the influence available, and randomly spawns after

--- src/examplefuncsplayer/RobotPlayer.java
+++ src/examplefuncsplayer/RobotPlayer.java
@@ -62,6 +62,17 @@ public strictfp class RobotPlayer {
     }
 
     static void runEnlightenmentCenter() throws GameActionException {
+        int currInfluence = rc.getInfluence();
+        int biddingInfluence = currInfluence / 10;
+        assert rc.canBid(biddingInfluence);
+        if (rc.canBid(biddingInfluence) && rc.getRoundNum() > 200) {
+            rc.bid(biddingInfluence);
+        }
+        System.out.println("curr influence after bid:" + rc.getInfluence());
+        System.out.println("bidding influence:" + biddingInfluence);
+        System.out.println();
+
+
         RobotType toBuild = randomSpawnableRobotType();
         int influence = 50;
         for (Direction dir : directions) {
