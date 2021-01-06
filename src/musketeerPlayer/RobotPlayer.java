1f4594302f1b69dfff1b27693b5fb81e007ed111
maxwelljones14 <56135118+maxwelljones14@users.noreply.github.com>
2021-01-05 19:23:48 -0500

    updates
    brief updates

--- src/musketeerPlayer/RobotPlayer.java
+++ src/musketeerPlayer/RobotPlayer.java
@@ -38,14 +38,14 @@ public strictfp class RobotPlayer {
 
         turnCount = 0;
 
-        System.out.println("I'm a " + rc.getType() + " and I just got created!");
+        //System.out.println("I'm a " + rc.getType() + " and I just got created!");
         while (true) {
             turnCount += 1;
             // Try/catch blocks stop unhandled exceptions, which cause your robot to freeze
             try {
                 // Here, we've separated the controls into a different method for each RobotType.
                 // You may rewrite this into your own control structure if you wish.
-                System.out.println("I'm a " + rc.getType() + "! Location " + rc.getLocation());
+                //System.out.println("I'm a " + rc.getType() + "! Location " + rc.getLocation());
                 switch (rc.getType()) {
                     case ENLIGHTENMENT_CENTER: runEnlightenmentCenter(); break;
                     case POLITICIAN:           runPolitician();          break;
@@ -57,13 +57,14 @@ public strictfp class RobotPlayer {
                 Clock.yield();
 
             } catch (Exception e) {
-                System.out.println(rc.getType() + " Exception");
+                //System.out.println(rc.getType() + " Exception");
                 e.printStackTrace();
             }
         }
     }
 
     static void runEnlightenmentCenter() throws GameActionException {
+        System.out.println("AI current influence: " + rc.getInfluence());
         int currRoundNum = rc.getRoundNum();
         int currInfluence = rc.getInfluence();
         int biddingInfluence = currInfluence / 10;
@@ -71,9 +72,12 @@ public strictfp class RobotPlayer {
         if (rc.canBid(biddingInfluence) && currRoundNum > 200) {
             rc.bid(biddingInfluence);
         }
-        System.out.println("curr influence after bid:" + rc.getInfluence());
-        System.out.println("bidding influence:" + biddingInfluence);
-        System.out.println();
+        else {
+            rc.bid(1);
+        }
+        //System.out.println("curr influence after bid:" + rc.getInfluence());
+        //System.out.println("bidding influence:" + biddingInfluence);
+        //System.out.println();
         RobotType toBuild;
         int influence;
         if (currRoundNum > 500) {
@@ -102,38 +106,29 @@ public strictfp class RobotPlayer {
         for (Direction dir : directions) {
             if (rc.canBuildRobot(toBuild, dir, influence)) {
                 rc.buildRobot(toBuild, dir, influence);
-                robotCounter++;
+                robotCounter+=1;
             } else {
                 break;
             }
         }
     }
     static void runPolitician() throws GameActionException {
-        boolean kill = Math.random() > 0.5;
-        if (turnCount == 1) {
-            if (rc.canEmpower(actionRadius)) {
-                System.out.println("empowering...");
-                rc.empower(actionRadius);
-                System.out.println("empowered");
-                return;
-            }  
-        }
         Team enemy = rc.getTeam().opponent();
         int actionRadius = rc.getType().actionRadiusSquared;
         RobotInfo[] attackable = rc.senseNearbyRobots(actionRadius, enemy);
         if (attackable.length != 0 && rc.canEmpower(actionRadius)) {
-            System.out.println("empowering...");
+            //System.out.println("empowering...");
             rc.empower(actionRadius);
-            System.out.println("empowered");
+            //System.out.println("empowered");
             return;
         }
-        if (tryMove(randomDirection()))
-            System.out.println("I moved!");
+        if (tryMove(randomDirection()));
+            //System.out.println("I moved!");
     }
 
     static void runSlanderer() throws GameActionException {
-        if (tryMove(randomDirection()))
-            System.out.println("I moved!");
+        if (tryMove(randomDirection()));
+            //System.out.println("I moved!");
     }
 
     static void runMuckraker() throws GameActionException {
@@ -143,14 +138,14 @@ public strictfp class RobotPlayer {
             if (robot.type.canBeExposed()) {
                 // It's a slanderer... go get them!
                 if (rc.canExpose(robot.location)) {
-                    System.out.println("e x p o s e d");
+                    //System.out.println("e x p o s e d");
                     rc.expose(robot.location);
                     return;
                 }
             }
         }
-        if (tryMove(randomDirection()))
-            System.out.println("I moved!");
+        if (tryMove(randomDirection()));
+            //System.out.println("I moved!");
     }
 
     /**
@@ -179,7 +174,7 @@ public strictfp class RobotPlayer {
      * @throws GameActionException
      */
     static boolean tryMove(Direction dir) throws GameActionException {
-        System.out.println("I am trying to move " + dir + "; " + rc.isReady() + " " + rc.getCooldownTurns() + " " + rc.canMove(dir));
+        //System.out.println("I am trying to move " + dir + "; " + rc.isReady() + " " + rc.getCooldownTurns() + " " + rc.canMove(dir));
         if (rc.canMove(dir)) {
             rc.move(dir);
             return true;
