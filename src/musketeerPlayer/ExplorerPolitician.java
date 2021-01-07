b3cb75e5f0ecb875c0291223d92d8ba76fff707d
Bharath Sreenivas <sreenivas.bharath@gmail.com>
2021-01-06 21:54:28 -0600

    added explorer politicians

--- src/musketeerPlayer/ExplorerPolitician.java
+++ src/musketeerPlayer/ExplorerPolitician.java
@@ -3,26 +3,17 @@ import battlecode.common.*;
 
 import musketeerplayer.Util.*;
 
-public class Politician extends Robot {
+public class ExplorerPolitician extends Robot {
     static Direction main_direction;
     
-    public Politician(RobotController r) {
+    public ExplorerPolitician(RobotController r) {
         super(r);
-        try {
-            if (rc.canSetFlag(0)) {
-                rc.setFlag(0);
-            }
-        }
-        catch (Exception e) {
-            System.out.println(rc.getType() + " Exception");
-            e.printStackTrace();
-        }
     }
 
     public void takeTurn() throws GameActionException {
         super.takeTurn();
 
-        System.out.println("I am a " + rc.getType() + "; current influence: " + rc.getInfluence());
+        System.out.println("I am a explorer politician; current influence: " + rc.getInfluence());
         System.out.println("current buff: " + rc.getEmpowerFactor(rc.getTeam(),0));
 
         if(main_direction == null){
@@ -53,29 +44,6 @@ public class Politician extends Robot {
         
         if (powerful != null) {
             Direction toMove = Util.findDirection(powerful.getLocation(), rc.getLocation());
-            tryMove(toMove);
-        }
-
-        RobotInfo[] within6 = rc.senseNearbyRobots(6, rc.getTeam());
-        for (RobotInfo robot : within6) {
-            if (robot.getType() == RobotType.POLITICIAN && rc.getFlag(robot.getID()) == 1) {
-                System.out.println("within radius 6");
-                return;
-            }
-        }
-        RobotInfo[] friendlySensable = rc.senseNearbyRobots(sensingRadius, rc.getTeam());
-        RobotInfo bestSlanderer = null;
-        max_influence = 0;
-        for (RobotInfo robot : friendlySensable) {
-            if (robot.getType() == RobotType.POLITICIAN && rc.getFlag(robot.getID()) == 1 && robot.getInfluence() > max_influence) {
-                System.out.println("within sensing radius but not 6");
-                max_influence = robot.getInfluence();
-                bestSlanderer = robot;
-            }
-        }
-
-        if (bestSlanderer != null) {
-            Direction toMove = Util.findDirection(bestSlanderer.getLocation(), rc.getLocation());
             tryMove(toMove);
         }
 
