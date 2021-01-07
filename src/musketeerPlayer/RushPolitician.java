12632dfe996391490dd5947ebeda51c34cd56ce1
maxwelljones14 <56135118+maxwelljones14@users.noreply.github.com>
2021-01-07 17:44:04 -0500

    changed behavior around neutral towers

--- src/musketeerPlayer/RushPolitician.java
+++ src/musketeerPlayer/RushPolitician.java
@@ -6,6 +6,7 @@ import musketeerplayer.Util.*;
 public class RushPolitician extends Robot {
     static MapLocation enemyLocation;
     static Direction main_direction;
+    static boolean   toDetonate = false;
     
     public RushPolitician(RobotController r, MapLocation enemyLoc) {
         super(r);
@@ -22,29 +23,56 @@ public class RushPolitician extends Robot {
         if(main_direction == null){
             main_direction = Util.randomDirection();
         }
+        
+
         Team enemy = rc.getTeam().opponent();
         int actionRadius = rc.getType().actionRadiusSquared;
+        int sensorRadius = rc.getType().sensorRadiusSquared;
+        RobotInfo[] friendlies = rc.senseNearbyRobots(sensorRadius, rc.getTeam());
         RobotInfo[] attackable = rc.senseNearbyRobots(actionRadius, enemy);
         RobotInfo[] neutrals = rc.senseNearbyRobots(actionRadius, Team.NEUTRAL);
-        for(RobotInfo robot: neutrals) {
-            if(robot.getType() == RobotType.ENLIGHTENMENT_CENTER && rc.canEmpower(actionRadius)){
-            //System.out.println("empowering...");
-            rc.empower(actionRadius);
-            //System.out.println("empowered");
-            return;
+        
+        if(toDetonate) {
+            for(RobotInfo robot: neutrals) {
+                if(robot.getType() == RobotType.ENLIGHTENMENT_CENTER && rc.canEmpower(actionRadius)){
+                    rc.empower(actionRadius);
+                }
+            }
+
+        }
+        else {
+            for(RobotInfo robot: neutrals) {
+                if(robot.getType() == RobotType.ENLIGHTENMENT_CENTER){
+                    int ECHealth = (int)(robot.getConviction() * 1.25);
+                    for(RobotInfo friendlyRobot: friendlies) {
+                        if(friendlyRobot.getType() == RobotType.POLITICIAN && Util.distanceSquared(friendlyRobot.getLocation(), robot.getLocation()) <= sensorRadius) {
+                            ECHealth -= friendlyRobot.getInfluence();
+                            ECHealth += 10;
+                        }
+                    }
+                    if(ECHealth <= 0) {
+                        setFlag(Comms.getFlag(Comms.InformationCategory.DETONATE, 0, 0));
+                    }
+                }
+            }
+
+            for(RobotInfo robot: friendlies) {
+                if(robot.getType() == RobotType.POLITICIAN && Comms.getIC(rc.getFlag(robot.getID())) == Comms.InformationCategory.DETONATE) {
+                    toDetonate = true;
+                }
+            }
+            
+            for(RobotInfo robot: attackable) {
+                if(robot.getType() == RobotType.ENLIGHTENMENT_CENTER && rc.canEmpower(actionRadius)){
+                    //System.out.println("empowering...");
+                    rc.empower(actionRadius);
+                    //System.out.println("empowered");
+                    return;
+                }
             }
         }
-        for(RobotInfo robot: attackable) {
-            if(robot.getType() == RobotType.ENLIGHTENMENT_CENTER && rc.canEmpower(actionRadius)){
-                //System.out.println("empowering...");
-                rc.empower(actionRadius);
-                //System.out.println("empowered");
-                return;
-            }
-        }
-        int sensorRadius = rc.getType().sensorRadiusSquared;
-        RobotInfo[] sensable = rc.senseNearbyRobots(sensorRadius, rc.getTeam());
-        for (RobotInfo robot : sensable) {
+
+        for (RobotInfo robot : friendlies) {
             if (robot.getType() == RobotType.ENLIGHTENMENT_CENTER && enemyLocation.equals(robot.getLocation())) {
                 changeTo = new ExplorerPolitician(rc, dx, dy);
                 if (rc.getRoundNum() % 3 == 0) {
