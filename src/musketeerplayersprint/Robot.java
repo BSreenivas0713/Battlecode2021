ac6dcce80dad94e15a18f289d8a91f067ec1eccb
maxwelljones14 <56135118+maxwelljones14@users.noreply.github.com>
2021-01-10 14:17:25 -0500

    Added musketeerplayersprint, a version of musketeerplayer that we will most likely use for the sprint tournament

---
+++ src/musketeerplayersprint/Robot.java
@@ -0,0 +1,150 @@
+package musketeerplayersprint;
+import battlecode.common.*;
+
+import musketeerplayersprint.Util.*;
+import musketeerplayersprint.Comms.*;
+
+public class Robot {
+    static RobotController rc;
+    static int turnCount = 0;
+
+    public static int dx = Util.dOffset;
+    public static int dy = Util.dOffset;
+    static int defaultFlag;
+    static int nextFlag;
+    static boolean resetFlagOnNewTurn = true;
+    static int sensorRadius;
+    static int actionRadius;
+    static Team enemy;
+    static RobotInfo[] enemySensable;
+    static RobotInfo[] friendlySensable;
+    static RobotInfo[] neutralSensable;
+    static RobotInfo[] enemyAttackable;
+
+    public static Robot changeTo = null;
+
+    public Robot(RobotController r) {
+        rc = r;
+        enemy = rc.getTeam().opponent();
+        sensorRadius = rc.getType().sensorRadiusSquared;
+        actionRadius = rc.getType().actionRadiusSquared;
+        defaultFlag = 0;
+        RobotInfo[] sensable = rc.senseNearbyRobots(sensorRadius, rc.getTeam());
+        MapLocation currLoc = rc.getLocation();
+        for (RobotInfo robot : sensable) {
+            if (robot.getType() == RobotType.ENLIGHTENMENT_CENTER) {
+                MapLocation ecLoc = robot.getLocation();
+                dx += currLoc.x - ecLoc.x;
+                dy += currLoc.y - ecLoc.y;
+            }
+        }
+
+        nextFlag = Comms.getFlag(InformationCategory.NEW_ROBOT);
+    }
+
+    public Robot(RobotController r, int currDx, int currDy) {
+        enemy = rc.getTeam().opponent();
+        sensorRadius = rc.getType().sensorRadiusSquared;
+        actionRadius = rc.getType().actionRadiusSquared;
+        rc = r;
+        defaultFlag = 0;
+        dx = currDx;
+        dy = currDy;
+    }
+
+    public void takeTurn() throws GameActionException {
+        initializeGlobals();
+        turnCount += 1;
+        if(rc.getFlag(rc.getID()) != nextFlag) {
+            setFlag(nextFlag);
+        }
+        Util.vPrintln("Flag set: " + Integer.toBinaryString(rc.getFlag(rc.getID())));
+
+        if(resetFlagOnNewTurn)
+            nextFlag = defaultFlag;
+    }
+
+    public void initializeGlobals() throws GameActionException {
+        enemySensable = rc.senseNearbyRobots(sensorRadius, enemy);
+        friendlySensable = rc.senseNearbyRobots(sensorRadius, rc.getTeam());
+        neutralSensable = rc.senseNearbyRobots(sensorRadius, Team.NEUTRAL);
+        enemyAttackable  = rc.senseNearbyRobots(actionRadius, enemy);
+    }
+
+    /**
+     * Attempts to move in a given direction.
+     *
+     * @param dir The intended direction of movement
+     * @return true if a move was performed
+     * @throws GameActionException
+     */
+    static boolean tryMove(Direction dir) throws GameActionException {
+        //Util.vPrintln("I am trying to move " + dir + "; " + rc.isReady() + " " + rc.getCooldownTurns() + " " + rc.canMove(dir));
+        if (rc.canMove(dir)) {
+            rc.move(dir);
+            dx += dir.getDeltaX();
+            dy += dir.getDeltaY();
+            return true;
+        } else return false;
+    }
+
+    static boolean tryMoveDest(Direction target_dir) throws GameActionException {
+        // Util.vPrintln("Dest direction: " + dir);
+        Direction[] dirs = {target_dir, target_dir.rotateRight(), target_dir.rotateLeft(), 
+            target_dir.rotateRight().rotateRight(), target_dir.rotateLeft().rotateLeft()};
+
+        for(Direction dir : dirs) {
+            if(rc.canMove(dir)) {
+                rc.move(dir);
+                
+                dx += dir.getDeltaX();
+                dy += dir.getDeltaY();
+    
+                return true;
+            }
+        }
+        
+        return false;
+    }
+
+    /**
+     * @return true if found an EC and broadcasted
+     */
+    boolean broadcastECLocation() {
+        boolean res = false;
+
+        RobotInfo[] sensable = rc.senseNearbyRobots(sensorRadius);
+        for (RobotInfo robot : sensable) {
+            if(robot.getTeam() != rc.getTeam() && 
+               robot.getType() == RobotType.ENLIGHTENMENT_CENTER && 
+               robot.getConviction() <= Util.minECRushConviction) {
+                res = true;
+
+                MapLocation currLoc = rc.getLocation();
+                MapLocation ecLoc = robot.getLocation();
+
+                int ecDX = dx + ecLoc.x - currLoc.x;
+                int ecDY = dy + ecLoc.y - currLoc.y;
+
+                int flag = 0;
+                int inf = (int) Math.min(31, Math.ceil(Math.log(robot.getInfluence()) / Math.log(Comms.INF_LOG_BASE)));
+                if(robot.getTeam() == enemy) {
+                    flag = Comms.getFlag(InformationCategory.ENEMY_EC, inf, ecDX, ecDY);
+                } else {
+                    flag = Comms.getFlag(InformationCategory.NEUTRAL_EC, inf, ecDX, ecDY);
+                }
+
+                nextFlag = flag;
+            }
+        }
+        return res;
+    }
+
+    void setFlag(int flag) {
+        try {
+            if(rc.canSetFlag(flag)) {
+                rc.setFlag(flag);
+            }
+        } catch (Exception e) {}
+    }
+}
