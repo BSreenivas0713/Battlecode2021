50ac2a28baa49ab7dd60398cdd507988ba7d104d
Bharath Sreenivas <sreenivas.bharath@gmail.com>
2021-01-10 23:35:33 -0600

    Removed checking if isReady to enter rush mode, removed sendTroopsSemaphore since we only rush 1 bot now, made slanderers check if their target location is on the map, changed remove_blockage politician influence to 30 instead of 20, go back to saving for rush in rushing if rush bug is encountered

--- src/musketeerplayersprint/Slanderer.java
+++ src/musketeerplayersprint/Slanderer.java
@@ -71,10 +71,12 @@ public class Slanderer extends Robot {
         double currPass;
         int i = 0;
         while (i < 8) {
-            currPass = rc.sensePassability(curr.add(tempDir));
-            if (currPass > maxPass) {
-                maxPass = currPass;
-                maxDir = tempDir;
+            if (rc.onTheMap(curr.add(tempDir))) {
+                currPass = rc.sensePassability(curr.add(tempDir));
+                if (currPass > maxPass) {
+                    maxPass = currPass;
+                    maxDir = tempDir;
+                }
             }
             i++;
             tempDir = tempDir.rotateRight();
