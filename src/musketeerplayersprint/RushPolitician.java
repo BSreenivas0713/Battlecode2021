e166ada10a3944c3e5a023ba6c18c759ab25a211
David Lyons <dalyons@andrew.cmu.edu>
2021-01-11 21:45:15 -0500

    Fixed rush politicians to only empower minimum necessary radius and also to get as close as possible using a move semaphore.

--- src/musketeerplayersprint/RushPolitician.java
+++ src/musketeerplayersprint/RushPolitician.java
@@ -9,10 +9,12 @@ public class RushPolitician extends Robot {
     static MapLocation enemyLocation;
     static Direction main_direction;
     static boolean   toDetonate = false;
+    static int moveSemaphore;
     
     public RushPolitician(RobotController r, MapLocation enemyLoc) {
         super(r);
         enemyLocation = enemyLoc;
+        moveSemaphore = 5;
         defaultFlag = Comms.getFlag(Comms.InformationCategory.ROBOT_TYPE, Comms.SubRobotType.POL_RUSH);
     }
 
@@ -20,6 +22,7 @@ public class RushPolitician extends Robot {
         super(r);
         enemyLocation = enemyLoc;
         toDetonate = det;
+        moveSemaphore = 5;
         defaultFlag = Comms.getFlag(Comms.InformationCategory.ROBOT_TYPE, Comms.SubRobotType.POL_RUSH);
     }
 
@@ -29,19 +32,20 @@ public class RushPolitician extends Robot {
         Debug.println(Debug.info, "I am a rush politician; current influence: " + rc.getInfluence());
         Debug.println(Debug.info, "current buff: " + rc.getEmpowerFactor(rc.getTeam(),0));
         Debug.println(Debug.info, "target map location: x:" + enemyLocation.x + ", y:" + enemyLocation.y);
-
-        if(main_direction == null){
-            main_direction = Util.randomDirection();
-        }
-
+        
+        MapLocation currLoc = rc.getLocation();
         RobotInfo[] neutrals = rc.senseNearbyRobots(actionRadius, Team.NEUTRAL);
+        boolean targetFound = false;
         
         for(RobotInfo robot : enemyAttackable) {
             MapLocation loc = robot.getLocation();
             if(robot.getType() == RobotType.ENLIGHTENMENT_CENTER && 
-                enemyLocation.isWithinDistanceSquared(loc, 8) &&
-                rc.canEmpower(actionRadius)) {
-                rc.empower(actionRadius);
+                enemyLocation.isWithinDistanceSquared(loc, 8)) {
+                targetFound = true;
+                int dist = currLoc.distanceSquaredTo(loc);
+                if (rc.canEmpower(dist) && (moveSemaphore == 0 || currLoc.isAdjacentTo(loc))) {
+                    rc.empower(dist);
+                }
                 return;
             }
         }
@@ -49,24 +53,37 @@ public class RushPolitician extends Robot {
         for(RobotInfo robot : neutrals) {
             MapLocation loc = robot.getLocation();
             if(robot.getType() == RobotType.ENLIGHTENMENT_CENTER && 
-                enemyLocation.isWithinDistanceSquared(loc, 8) &&
-                rc.canEmpower(actionRadius)) {
-                rc.empower(actionRadius);
+                enemyLocation.isWithinDistanceSquared(loc, 8)) {
+                targetFound = true;
+                int dist = currLoc.distanceSquaredTo(loc);
+                if (rc.canEmpower(dist) && (moveSemaphore == 0 || currLoc.isAdjacentTo(loc))) {
+                    rc.empower(dist);
+                }
                 return;
             }
         }
 
-        for (RobotInfo robot : friendlySensable) {
-            MapLocation loc = robot.getLocation();
-            if (robot.getType() == RobotType.ENLIGHTENMENT_CENTER && enemyLocation.isWithinDistanceSquared(loc, 2) &&
-                loc.isWithinDistanceSquared(rc.getLocation(), actionRadius)) {
-                changeTo = new GolemPolitician(rc);
-                return;
+        if (!targetFound) {
+            for (RobotInfo robot : friendlySensable) {
+                MapLocation loc = robot.getLocation();
+                if (robot.getType() == RobotType.ENLIGHTENMENT_CENTER && enemyLocation.isWithinDistanceSquared(loc, 8) &&
+                    loc.isWithinDistanceSquared(currLoc, actionRadius)) {
+                    changeTo = new GolemPolitician(rc);
+                    return;
+                }
             }
         }
 
         main_direction = rc.getLocation().directionTo(enemyLocation);
-        if(!rc.getLocation().isWithinDistanceSquared(enemyLocation, 2)){
+        if (currLoc.isAdjacentTo(enemyLocation)) {
+            moveSemaphore = 0;
+        } else if (currLoc.distanceSquaredTo(enemyLocation) <= actionRadius) {
+            if (tryMove(main_direction)) {
+                moveSemaphore = 5;
+            } else {
+                moveSemaphore--;
+            }
+        } else {
             tryMoveDest(main_direction);
         }
         
