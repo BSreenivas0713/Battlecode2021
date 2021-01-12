98ed9243dc538658f32116a57f754e446e74f82d
Winston Cheung <Winston.cheung1@gmail.com>
2021-01-11 20:47:10 -0500

    Changed slanderer influences to take advantage of local optimizations of profit

---
+++ precompuation.py
@@ -0,0 +1,22 @@
+import math
+
+def calculate_profit(x):
+    return math.floor(x * (0.02 + 0.03 * math.exp(-0.001 * x))) * 50 - x
+
+def calculate_best_profits():
+    length = 950;
+    dp = [-100] * length;
+    dp[0] = calculate_profit(0);
+    for x in range(1, length):
+        dp[x] = x if calculate_profit(x) > calculate_profit(dp[x-1]) else dp[x-1]
+    
+    return dp;
+
+
+
+if __name__ == '__main__':
+    dp = calculate_best_profits()
+    for i in range(0, len(dp)):
+        dp[i] = chr(dp[i])
+    s = "".join(dp)
+    print(s)
