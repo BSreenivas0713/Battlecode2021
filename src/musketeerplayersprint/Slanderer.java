38021c60500ec657be140a7c4b567790a5787735
David Lyons <dalyons@andrew.cmu.edu>
2021-01-12 16:06:04 -0500

    Made it so slanderers that aren't making money run away from the base.

--- src/musketeerplayersprint/Slanderer.java
+++ src/musketeerplayersprint/Slanderer.java
@@ -28,11 +28,90 @@ public class Slanderer extends Robot {
         Debug.println(Debug.info, "I am a slanderer; current influence: " + rc.getInfluence());
         Debug.println(Debug.info, "current buff: " + rc.getEmpowerFactor(rc.getTeam(),0));
 
+        MapLocation curr = rc.getLocation();
+
+        if (turnCount <= 50) {
+            if(main_direction == null) {
+                main_direction = Util.randomDirection();
+            }
+
+            RobotInfo[] neutralECs = rc.senseNearbyRobots(sensorRadius, Team.NEUTRAL);
+            RobotInfo minRobot = null;
+            double minDistSquared = Integer.MAX_VALUE;
+            for (RobotInfo robot : enemySensable) {
+                double temp = curr.distanceSquaredTo(robot.getLocation());
+                if (temp < minDistSquared) {
+                    minDistSquared = temp;
+                    minRobot = robot;
+                }
+            }
+            
+            RobotInfo minNeutralRobot = null;
+            double minNeutralDistSquared = Integer.MAX_VALUE;
+            for (RobotInfo robot: neutralECs) {
+                double temp = curr.distanceSquaredTo(robot.getLocation());
+                if (temp < minNeutralDistSquared) {
+                    minNeutralDistSquared = temp;
+                    minNeutralRobot = robot;
+                }
+            }
+            
+            RobotInfo friendlyEC = null;
+            for (RobotInfo robot: friendlySensable) {
+                if (robot.getType() == RobotType.ENLIGHTENMENT_CENTER) {
+                    friendlyEC = robot;
+                }
+            }
+
+            boolean moveBack = false;
+            if (!curr.isWithinDistanceSquared(home, 2 * sensorRadius)) {
+                moveBack = true;
+            }
+
+            double maxPass = 0;
+            Direction maxDir = null;
+            Direction tempDir = Util.randomDirection();
+            double currPass;
+            int i = 0;
+            while (i < 8) {
+                if (rc.onTheMap(curr.add(tempDir))) {
+                    currPass = rc.sensePassability(curr.add(tempDir));
+                    if (currPass > maxPass) {
+                        maxPass = currPass;
+                        maxDir = tempDir;
+                    }
+                }
+                i++;
+                tempDir = tempDir.rotateRight();
+            }
+            main_direction = maxDir;
+
+            if (minRobot != null) {
+                main_direction = curr.directionTo(minRobot.getLocation()).opposite();
+            }
+            else if (minNeutralRobot != null) {
+                main_direction = curr.directionTo(minNeutralRobot.getLocation()).opposite(); 
+            }
+            else if (friendlyEC != null) {
+                main_direction = curr.directionTo(friendlyEC.getLocation()).opposite(); 
+            } else if (moveBack == true) {
+                main_direction = curr.directionTo(home);
+            }
+
+            MapLocation target = rc.adjacentLocation(main_direction);
+            if (rc.onTheMap(target)) {
+                while (!tryMove(main_direction) && rc.isReady()) {
+                    main_direction = Util.randomDirection();
+                }
+            }
+
+            broadcastECLocation();
+        }
+    } else {
         if(main_direction == null) {
             main_direction = Util.randomDirection();
         }
 
-        RobotInfo[] neutralECs = rc.senseNearbyRobots(sensorRadius, Team.NEUTRAL);
         RobotInfo minRobot = null;
         double minDistSquared = Integer.MAX_VALUE;
         MapLocation curr = rc.getLocation();
@@ -43,57 +122,10 @@ public class Slanderer extends Robot {
                 minRobot = robot;
             }
         }
-        
-        RobotInfo minNeutralRobot = null;
-        double minNeutralDistSquared = Integer.MAX_VALUE;
-        for (RobotInfo robot: neutralECs) {
-            double temp = curr.distanceSquaredTo(robot.getLocation());
-            if (temp < minNeutralDistSquared) {
-                minNeutralDistSquared = temp;
-                minNeutralRobot = robot;
-            }
-        }
-        
-        RobotInfo friendlyEC = null;
-        for (RobotInfo robot: friendlySensable) {
-            if (robot.getType() == RobotType.ENLIGHTENMENT_CENTER) {
-                friendlyEC = robot;
-            }
-        }
-
-        boolean moveBack = false;
-        if (!curr.isWithinDistanceSquared(home, 2 * sensorRadius)) {
-            moveBack = true;
-        }
-
-        double maxPass = 0;
-        Direction maxDir = null;
-        Direction tempDir = Util.randomDirection();
-        double currPass;
-        int i = 0;
-        while (i < 8) {
-            if (rc.onTheMap(curr.add(tempDir))) {
-                currPass = rc.sensePassability(curr.add(tempDir));
-                if (currPass > maxPass) {
-                    maxPass = currPass;
-                    maxDir = tempDir;
-                }
-            }
-            i++;
-            tempDir = tempDir.rotateRight();
-        }
-        main_direction = maxDir;
-
         if (minRobot != null) {
             main_direction = curr.directionTo(minRobot.getLocation()).opposite();
-        }
-        else if (minNeutralRobot != null) {
-            main_direction = curr.directionTo(minNeutralRobot.getLocation()).opposite(); 
-        }
-        else if (friendlyEC != null) {
-            main_direction = curr.directionTo(friendlyEC.getLocation()).opposite(); 
-        } else if (moveBack == true) {
-            main_direction = curr.directionTo(home);
+        } else if (curr.isWithinDistanceSquared(home, 2 * sensorRadius)) {
+            main_direction = curr.directionTo(home).opposite();
         }
 
         MapLocation target = rc.adjacentLocation(main_direction);
