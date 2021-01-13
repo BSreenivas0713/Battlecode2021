4ba2235ed4d2624e3731b4a56140c91142ab5651
maxwelljones14 <56135118+maxwelljones14@users.noreply.github.com>
2021-01-12 22:58:35 -0500

    Making naive protector politicians: still very buggy

--- src/musketeerplayersprint2/Politician.java
+++ src/musketeerplayersprint2/Politician.java
@@ -3,124 +3,113 @@ import battlecode.common.*;
 
 import musketeerplayersprint2.Util.*;
 import musketeerplayersprint2.Debug.*;
-
+/*1. blow up if around a muckraker if we also sense base/a slanderer
+2. blow up if around 2 or 3 muckrakers 
+3. push enemy muckrakers away from base/where slanderers are if we are within 2 sensor radiuses of base or we see a slanderer
+4. if within sensor radius of base move away
+5. if in sensor radius of base make a move that keeps you most closely within sensor radius of base(in one direction)
+6. if too far away from base move back towards base
+EXTRA CREDIT: move towards muckrakers near slanderers if when slanderers signal that they are in trouble */
 public class Politician extends Robot {
+    static RotationDirection spinDirection = Util.RotationDirection.COUNTERCLOCKWISE;
     static Direction main_direction;
     static final int slandererFlag = Comms.getFlag(Comms.InformationCategory.ROBOT_TYPE, Comms.SubRobotType.SLANDERER);
     
     public Politician(RobotController r) {
         super(r);
-        defaultFlag = Comms.getFlag(Comms.InformationCategory.ROBOT_TYPE, Comms.SubRobotType.POL_BODYGUARD);
+        defaultFlag = Comms.getFlag(Comms.InformationCategory.ROBOT_TYPE, Comms.SubRobotType.POL_PROTECTOR);
     }
 
     public void takeTurn() throws GameActionException {
         super.takeTurn();
 
-        Debug.println(Debug.info, "I am a normal politician; current influence: " + rc.getInfluence());
+        Debug.println(Debug.info, "I am a protector politician; current influence: " + rc.getInfluence());
         Debug.println(Debug.info, "current buff: " + rc.getEmpowerFactor(rc.getTeam(),0));
 
         RobotInfo[] neutrals = rc.senseNearbyRobots(actionRadius, Team.NEUTRAL);
-        RobotInfo[] within6 = rc.senseNearbyRobots(6, rc.getTeam());
-
-        if(main_direction == null){
-            main_direction = Util.randomDirection();
-        }
-        
         MapLocation currLoc = rc.getLocation();
-        int maxEnemyDistSquared = Integer.MIN_VALUE;
-        MapLocation farthestEnemy = null;
+
+        main_direction = Util.rightOrLeftTurn(spinDirection, home.directionTo(currLoc)); //Direction if we only want to rotate around the base
+        /* Creating all the variables that we need to do the step by step decision making for later*/
+
+        boolean slandererOrECNearby = false;
+        boolean ECNearby = false;
+        for (RobotInfo robot : friendlySensable) {
+            if ((robot.getType() == RobotType.ENLIGHTENMENT_CENTER)){
+                slandererOrECNearby = true;
+                ECNearby = true;
+            } 
+            
+            if((rc.canGetFlag(robot.getID()) && 
+                rc.getFlag(robot.getID()) == slandererFlag)) {
+                slandererOrECNearby = true;
+            }
+        }
+
+        int distanceToEC = rc.getLocation().distanceSquaredTo(home);
+        
+        int maxEnemyAttackableDistSquared = Integer.MIN_VALUE;
+        MapLocation farthestEnemyAttackable = null;
         for (RobotInfo robot : enemyAttackable) {
             int temp = currLoc.distanceSquaredTo(robot.getLocation());
-            if (temp > maxEnemyDistSquared) {
-                maxEnemyDistSquared = temp;
-                farthestEnemy = robot.getLocation();
+            if (temp > maxEnemyAttackableDistSquared) {
+                maxEnemyAttackableDistSquared = temp;
+                farthestEnemyAttackable = robot.getLocation();
             }
         }
 
-        if (enemyAttackable.length > 0 && rc.canEmpower(maxEnemyDistSquared)) {
-            Debug.println(Debug.info, "Empowered with radius: " + maxEnemyDistSquared);
-            Debug.setIndicatorLine(rc.getLocation(), farthestEnemy, 255, 150, 50);
-            rc.empower(maxEnemyDistSquared);
+        RobotInfo closestMuckrakerSensable = null;
+        int minDistance = Integer.MAX_VALUE;
+
+        for (RobotInfo robot : enemySensable) {
+            int currDistance = robot.getLocation().distanceSquaredTo(home);
+            if (robot.getType() == RobotType.MUCKRAKER && currDistance < minDistance) {
+                closestMuckrakerSensable = robot;
+                minDistance = currDistance;
+            }
+        }
+        
+        /* Step by Step decision making*/
+
+        //empower if near 2 enemies or near an enemy while a slanderer or EC is nearby
+        if ((enemyAttackable.length > 1 || (enemyAttackable.length >0 && slandererOrECNearby)) && rc.canEmpower(maxEnemyAttackableDistSquared)) {
+            Debug.println(Debug.info, "Empowered with radius: " + maxEnemyAttackableDistSquared);
+            Debug.setIndicatorLine(rc.getLocation(), farthestEnemyAttackable, 255, 150, 50);
+            rc.empower(maxEnemyAttackableDistSquared);
             return;
         }
-
-        RobotInfo powerful = null;
-        int max_influence = 0;
-
-        MapLocation ECWithinSensable = null;
-
-        for (RobotInfo robot : enemySensable) {
-            int currInfluence = robot.getInfluence();
-            if (robot.getType() == RobotType.MUCKRAKER && currInfluence > max_influence) {
-                powerful = robot;
-                max_influence = currInfluence;
-            }
-            if (robot.getType() == RobotType.ENLIGHTENMENT_CENTER) {
-                ECWithinSensable = robot.getLocation();
-            }
-        }
-        
-        if (powerful != null) {
-            Direction toMove = rc.getLocation().directionTo(powerful.getLocation());
+        //tries to block a muckraker in its path(if the muckraker is within 2 sensing radiuses of the EC)
+        if (closestMuckrakerSensable != null && closestMuckrakerSensable.getLocation().distanceSquaredTo(home) <= 2 * sensorRadius ) {
+            Debug.println(Debug.info, "I am pushing a muckraker away");
+            MapLocation closestMuckrakerSensableLoc = closestMuckrakerSensable.getLocation();
+            Direction muckrakerPathtoBase = closestMuckrakerSensableLoc.directionTo(home);
+            MapLocation squareToBlock = closestMuckrakerSensableLoc.add(muckrakerPathtoBase);
+            Direction toMove = rc.getLocation().directionTo(squareToBlock);
             tryMoveDest(toMove);
+            return;
         }
-
-        boolean within2ofEC = false;
-        for (RobotInfo robot : rc.senseNearbyRobots(2, rc.getTeam())) {
-            if (robot.getType() == RobotType.ENLIGHTENMENT_CENTER) {
-                within2ofEC = true;
-            }
-        }
-
-        for (RobotInfo robot : within6) {
-            if (robot.getType() == RobotType.POLITICIAN && rc.getFlag(robot.getID()) == 1 && !within2ofEC) {
-                Debug.println(Debug.info, "within radius 6");
-                return;
-            }
-        }
-
-        RobotInfo bestSlanderer = null;
-        max_influence = 0;
-        for (RobotInfo robot : friendlySensable) {
-            if (rc.canGetFlag(robot.getID()) && 
-                rc.getFlag(robot.getID()) == slandererFlag && 
-                robot.getInfluence() > max_influence) {
-                Debug.println(Debug.info, "within sensing radius but not 6");
-                if (ECWithinSensable == null || robot.getLocation().distanceSquaredTo(ECWithinSensable) > robot.getType().sensorRadiusSquared) {
-                    max_influence = robot.getInfluence();
-                }
-                bestSlanderer = robot;
-            }
-        }
-
-        if (bestSlanderer != null) {
-            Direction toMove = rc.getLocation().directionTo(bestSlanderer.getLocation());
+        //moves out of sensor radius of Enlightenment Center
+        if (ECNearby) {
+            Debug.println(Debug.info, "I am moving away from the base");
+            Direction toMove = Util.rotateInSpinDirection(spinDirection, rc.getLocation().directionTo(home).opposite());
             tryMoveDest(toMove);
+            return;
         }
-        
-        RobotInfo weakest = null;
-        int min_influence = 0;
-        for (RobotInfo robot : enemySensable) {
-            int currInfluence = robot.getInfluence();
-            if (robot.getType() == RobotType.ENLIGHTENMENT_CENTER && currInfluence < min_influence) {
-                weakest = robot;
-                min_influence = currInfluence;
-            }
-        }
-        
-        if (weakest != null) {
-            Direction toMove = rc.getLocation().directionTo(weakest.getLocation());
+        //if too far away from sensor radius of Enlightenment center, move towards Enlgihtenment Center
+        if (distanceToEC > (int) (sensorRadius * 2)) {
+            Debug.println(Debug.info, "I am moving toward the base");
+            Direction toMove = rc.getLocation().directionTo(home);
             tryMoveDest(toMove);
+            return;
         }
-
-        if (!rc.getLocation().isWithinDistanceSquared(home, 2 * sensorRadius)) {
-            tryMoveDest(rc.getLocation().directionTo(home));
-        }
-        
-        while (!tryMove(main_direction) && rc.isReady()){
-            main_direction = Util.randomDirection();
+        //Rotates around the base
+        Debug.println(Debug.info, "I am rotating around the base");
+        while (!tryMoveDest(main_direction) && rc.isReady()){
+            Debug.println(Debug.info, "I am switching rotation direction");
+            spinDirection = Util.switchSpinDirection(spinDirection);
         }
 
         broadcastECLocation();
+        return;
     }
 }
