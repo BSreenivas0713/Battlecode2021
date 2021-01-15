8baf381d26898582c3bbc7c15670e1b013381462
Bharath Sreenivas <sreenivas.bharath@gmail.com>
2021-01-15 14:31:42 -0600

    removed onTheMap check when checking if we can sense all 8 squares around an enemy EC. canSenseLocation already returns false of newLocation is off the map

--- src/musketeerplayersprint2/HunterMuckracker.java
+++ src/musketeerplayersprint2/HunterMuckracker.java
@@ -191,7 +191,7 @@ public class HunterMuckracker extends Robot {
             }
             for(int x = 0; x < Util.directions.length; x++) {
                 MapLocation newLocation = enemyLocation.add(Util.directions[x]);
-                if(!rc.canSenseLocation(newLocation) && rc.onTheMap(newLocation)) {
+                if(!rc.canSenseLocation(newLocation)) {
                     canSenseAroundBase = false;
                     break;
                 }
