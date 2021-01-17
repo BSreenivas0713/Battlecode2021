cd543ed90b220625d73de5d3b99e138043ce1172
David Lyons <dalyons@andrew.cmu.edu>
2021-01-16 23:00:13 -0500

    Added a slanderer formation where they stay diagonal to each other within the sensor radius of the base.

--- src/musketeerplayersprint2/RobotPlayer.java
+++ src/musketeerplayersprint2/RobotPlayer.java
@@ -74,7 +74,7 @@ public strictfp class RobotPlayer {
                 bot = new LatticeProtector(rc);
                 break;
             case SLANDERER:
-                bot = new Slanderer(rc);
+                bot = new DiagonalSlanderer(rc);
                 break;
             case MUCKRAKER: 
                 for (RobotInfo robot : sensableWithin2) {
