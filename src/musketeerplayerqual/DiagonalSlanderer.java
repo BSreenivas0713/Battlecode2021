226ef97d2669510403236d9402857a1a9652e8f7
David Lyons <dalyons@andrew.cmu.edu>
2021-01-20 14:49:49 -0500

    Removed saving for rush from init so we don't have terrible influence levels.

--- src/musketeerplayerqual/DiagonalSlanderer.java
+++ src/musketeerplayerqual/DiagonalSlanderer.java
@@ -47,9 +47,7 @@ public class DiagonalSlanderer extends Robot {
         double minDistSquared = Integer.MAX_VALUE;
         double temp;
 
-        System.out.println("Entering the enemy sensable loop.");
         for(int i = enemySensable.length - 1; i >= 0; i--) {
-            System.out.println("Bytecodes left: " + Clock.getBytecodesLeft());
             robot = enemySensable[i];
             temp = curr.distanceSquaredTo(robot.getLocation());
             if (temp < minDistSquared) {
@@ -73,9 +71,7 @@ public class DiagonalSlanderer extends Robot {
         int id;
         int flag;
         int dist;
-        System.out.println("Entering the friendly sensable loop.");
         for(int i = friendlySensable.length - 1; i >= 0; i--) {
-            System.out.println("Bytecodes left: " + Clock.getBytecodesLeft());
             robot = friendlySensable[i];
             loc = robot.getLocation();
             id = robot.getID();
@@ -123,7 +119,6 @@ public class DiagonalSlanderer extends Robot {
         }
         MapLocation latticeLoc;
 
-        System.out.println("Entering movement. Bytecodes left: " + Clock.getBytecodesLeft());
         if(closestEnemy != null && curr.isWithinDistanceSquared(closestEnemy.getLocation(), Util.minDistFromEnemy)) {
             main_direction = curr.directionTo(closestEnemy.getLocation()).opposite();
             // flag = Comms.getFlag(Comms.InformationCategory.SLA_FLEEING, main_direction.ordinal());
@@ -173,14 +168,12 @@ public class DiagonalSlanderer extends Robot {
         MapLocation target = rc.adjacentLocation(main_direction);
 
         Debug.setIndicatorLine(Debug.pathfinding, curr, target, 100, 100, 255);
-        System.out.println("Broadcasting now... Bytecodes left: " + Clock.getBytecodesLeft());
         // if(avgEnemyDir != null && rc.getRoundNum() > avgEnemyDirTurn + Util.turnsEnemyBroadcastValid) {
         //     avgEnemyDir = null;
         //     resetFlagOnNewTurn = true;
         // }
 
         // This means that the first half of an EC-ID/EC-ID broadcast finished.
-        if(needToBroadcastHomeEC && rc.getFlag(rc.getID()) == defaultFlag) { broadcastHomeEC(); }
-        else if(foundOwnEnemy && broadcastEnemyLocalOrGlobal(closestEnemy.getLocation(), closestEnemyType));
+        if(foundOwnEnemy && broadcastEnemyLocalOrGlobal(closestEnemy.getLocation(), closestEnemyType));
     }
 }
