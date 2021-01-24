0d55483a3820949619b10cba565d003386ca43a8
Winston Cheung <Winston.cheung1@gmail.com>
2021-01-24 12:07:29 -0500

    Tweaking pol micro

--- src/musketeerplayerqual/BuffProtectorPolitician.java
+++ src/musketeerplayerqual/BuffProtectorPolitician.java
@@ -45,6 +45,8 @@ public class BuffProtectorPolitician extends Robot {
         int maxEnemyAttackableDistSquared = Integer.MIN_VALUE;
         MapLocation farthestEnemyAttackable = null;
         int maxAttackableSize = 0;
+        RobotInfo closestBuffMuck = null;
+        int closestBuffMuckDist = Integer.MAX_VALUE;
         for(int i = enemyAttackable.length - 1; i >= 0; i--) {
             robot = enemyAttackable[i];
             if (robot.getConviction() > maxAttackableSize) {
@@ -65,6 +67,12 @@ public class BuffProtectorPolitician extends Robot {
             if (robot.getType() == RobotType.MUCKRAKER && currDistance < minMuckrakerDistance) {
                 closestMuckrakerSensable = robot;
                 minMuckrakerDistance = currDistance;
+            }
+
+            if(robot.getType() == RobotType.MUCKRAKER &&
+                closestBuffMuckDist > currLoc.distanceSquaredTo(robot.getLocation())) {
+                closestBuffMuckDist = currLoc.distanceSquaredTo(robot.getLocation());
+                closestBuffMuck = robot;
             }
 
             int temp = currLoc.distanceSquaredTo(robot.getLocation());
@@ -109,19 +117,27 @@ public class BuffProtectorPolitician extends Robot {
         
         /* Step by Step decision making*/
         //empower if near 2 enemies or enemy is in sensing radius of our base
-        if ((maxAttackableSize >= rc.getInfluence() / 5)
-            && rc.canEmpower(maxEnemyAttackableDistSquared)) {
-            Debug.println(Debug.info, "Big enemy nearby");
-            Debug.setIndicatorLine(Debug.info, rc.getLocation(), farthestEnemyAttackable, 255, 150, 50);
-            rc.empower(maxEnemyAttackableDistSquared);
+        if ((closestBuffMuck != null && closestBuffMuck.getConviction() >= rc.getConviction() / 3)
+            && rc.canEmpower(closestBuffMuckDist)) {
+            Debug.println(Debug.info, "Big enemy nearby: Empowering with radius: " + closestBuffMuckDist);
+            Debug.setIndicatorLine(Debug.info, rc.getLocation(), closestBuffMuck.getLocation(), 255, 150, 50);
+            rc.empower(closestBuffMuckDist);
             return;
         }
 
         // This means that the first half of an EC-ID/EC-ID broadcast finished.
         if(closestEnemy != null && broadcastEnemyLocalOrGlobal(closestEnemy.getLocation(), closestEnemyType));
 
+        if(closestBuffMuck != null) {
+            Debug.println("Moving towards a buff muck");
+            Direction toMove = currLoc.directionTo(closestBuffMuck.getLocation());
+            Direction[] orderedDirs = Nav.greedyDirection(toMove, rc);
+            for(Direction dir : orderedDirs) {
+                tryMove(dir);
+            }
+        }
         //moves out of sensor radius of Enlightenment Center
-        if (ECNearby) {
+        else if (ECNearby) {
             Debug.println(Debug.info, "I am moving away from the base");
             Direction toMove = Util.rotateInSpinDirection(spinDirection, rc.getLocation().directionTo(home).opposite());
             tryMoveDest(toMove);
