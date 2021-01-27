1aa80921e1a4e15d2f36772444e45fa0fb7d1c46
Bharath Sreenivas <sreenivas.bharath@gmail.com>
2021-01-27 13:37:59 -0600

    added musketeerplayerpostqual, added null rushers back, made null rushers go to an EC if there is one during creation, otherwise they answer attack calls. This code is buggy since when we want to build protectors, we could still build null rushers if the influence matches up. need to test/fix

---
+++ src/musketeerplayerpostqual/fast/FastDirectionSet.java
@@ -0,0 +1,19 @@
+package musketeerplayerpostqual.fast;
+
+import battlecode.common.*;
+
+public class FastDirectionSet {
+    public int set;
+
+    public FastDirectionSet() {
+        set = 0;
+    }
+
+    public void add(Direction dir) {
+        set |= 1 << dir.ordinal();
+    }
+
+    public void remove(Direction dir) {
+        set &= ~(1 << dir.ordinal());
+    }
+}
